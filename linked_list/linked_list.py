87ad37cdb2c16be334af651fda39b10ddc2bc178
Greg Knudsen <dev.artist15@gmail.com>
2018-08-14 15:33:07 -0400

    linked list complete

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,44 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node( value )
+    if self.head == None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      self.tail.next_node = new_node
+      self.tail = new_node
+
 
   def remove_head(self):
-    pass
+    if self.head == None:
+      return None
+    elif self.head.get_next() is None:
+      node = self.head
+      self.head = None
+      self.tail = None
+    else:
+      node = self.head
+      self.head = node.get_next()
+    return node.get_value()
 
   def contains(self, value):
-    pass
+    cur_node = self.head
+    while cur_node:
+      if cur_node.get_value() == value:
+        return True
+      else:
+        cur_node = cur_node.get_next()
+    
 
   def get_max(self):
-    pass
+    if self.head == None:
+      return None
+    val = self.head.get_value()
+    cur_node = self.head.get_next()
+    while cur_node:
+      if cur_node.get_value() > val:
+        val = cur_node.get_value()
+      else:
+        cur_node = cur_node.get_next()
+    return val
