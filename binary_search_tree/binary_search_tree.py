79e6d932a4020260daba699044a431e67599af95
Charlie <ccariste@gmail.com>
2018-08-14 20:39:46 -0700

    CRC: all bst tests passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,38 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self.value == None:
+      self.value = BinarySearchTree(value)
+    elif value > self.value:
+      if self.right == None: 
+        self.right = BinarySearchTree(value)
+        return
+      else:
+        self.right.insert(value)
+    else: #value <= self.value:
+      if self.left == None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+        
 
   def contains(self, target):
-    pass
+    if self.value == None:
+      return False
+    if self.value == target:
+      return True
+    if target > self.value and self.right != None:
+      return self.right.contains(target)
+    if target <= self.value and self.left != None:
+      return self.left.contains(target)
+    return False
+
 
   def get_max(self):
-    pass
+    if self.value == None:
+      return None
+    if self.right == None:
+      return self.value
+    else: #self.right != None:
+      return self.right.get_max()
+    
