d3be6cb9c3f5b57f7cce0fff84cc2b3fc41f1900
Alexander <alexander.botello@g.austincc.edu>
2018-08-16 21:09:13 -0500

    Complete heap, passes all tests

--- heap/heap.py
+++ heap/heap.py
@@ -1,6 +1,6 @@
 class Heap:
   def __init__(self):
-    # storage starts with an unused 0 to make 
+    # storage starts with an unused 0 to make
     # integer division simpler later on
     self.storage = [0]
     self.size = 0
@@ -8,10 +8,10 @@ class Heap:
   def insert(self, value):
     # add to array
     self.storage.append( value )
-    # bubble _bubble_up
-    self._bubble_up( self.size + 1 )
     # # update size
     self.size += 1
+    # bubble _bubble_up
+    self._bubble_up( self.size  )
 
   def delete(self):
     # save value
@@ -22,7 +22,8 @@ class Heap:
     # return value
     retval = self.storage[1]
     self.storage[1] = self.storage[self.size]
-    self.size = self.size - 1
+    self.storage.pop()
+    self.size -= 1
     self._sift_down(1)
     return retval
 
@@ -32,31 +33,39 @@ class Heap:
     # find the biggest value
   def get_size(self):
     # return self.size
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    # if node smaller than parent
-    # while index // 2 > 0:
-    #   if self.storage[index] < self.storage[ index // 2 ]:
-    #   # while node > its arent
-    #     #swap
-    #     # swap parent a& valie @ index
-    #     # recursive call
-    #     tmp = self.storage[index // 2]
-    #     self.storage[index//2] = self.storage[index]
-    #     self.storage[index] = tmp
-    # index = index // 2
-    if self.storage[index] < self.storage[ (index // 2) ]:
-      return
-    else:
-      old_parent = self.storage[(index // 2)]
-      new_parent = self.storage[index]
-      self.storage[ (index // 2) ] = new_parent
-      self.storage[index] = old_parent
-    return
+    parent_index = index // 2
+    child = self.storage[index]
+    parent = self.storage[parent_index]
+
+    if child > parent and parent != 0:
+      self.storage[index] = parent
+      self.storage[parent_index] = child
+      self._bubble_up(parent_index)
 
   def _sift_down(self, index):
     # insert last element at (empty) root
     # while index < its children
       #swap
-    pass
+      while (index * 2) <= self.get_size():
+        min_child = self.get_min_child(index)
+        if self.storage[index] < self.storage[min_child]:
+          self.storage[index], self.storage[min_child] = self.storage[min_child], self.storage[index]
+        else:
+          break
+        index = min_child
+
+  def get_min_child(self, index):
+    left = index * 2
+    right = index * 2  + 1
+    if right > self.get_size():
+          return left
+    else:
+      if self.storage[left] > self.storage[right]:
+        return left
+      else:
+        return right
+
+
