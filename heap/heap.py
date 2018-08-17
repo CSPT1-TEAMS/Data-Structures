b7ec8d9998d5da0e0d528e98042c3c90f5a66832
unknown <alexisreyes90@gmail.com>
2018-08-16 22:33:15 -0400

    Heaps understood better, got up to sift down(not complete)

--- heap/heap.py
+++ heap/heap.py
@@ -1,3 +1,5 @@
+import math
+
 class Heap:
   def __init__(self):
     # storage starts with an unused 0 to make 
@@ -6,19 +8,50 @@ class Heap:
     self.size = 0
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+
+    self.size += 1
+
+    self._bubble_up(self.size)
 
   def delete(self):
-    pass
+    if self.size == 0:
+      return None
+
+    old_head = self.storage.pop(0)
+    new_head = self.storage.pop()
+    self.size -= 1
+    self.storage.insert(0, new_head)
+    return old_head
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return self.size
+    
 
   def _bubble_up(self, index):
-    pass
+    if self.storage[index] > self.storage[math.floor(index/ 2)]:
+      hold_num = self.storage[math.floor(index/ 2)]
+      self.storage[math.floor(index/ 2)] = self.storage[index]
+      self.storage[index] = hold_num
 
   def _sift_down(self, index):
-    pass
+    parent = self.storage[index]
+    left_tree = self.storage[(2*index) + 1]
+    right_tree = self.storage[(2*index) + 2]
+
+    if left_tree < right_tree:
+      bigger = right_tree
+      right_tree = parent
+      parent = bigger
+    else:
+      bigger = left_tree
+      left_tree = parent
+      parent = bigger
+
+
+
+
+    
